a7ea3b3e2f2cc3e4171cc16fe1fef2d3db259fdc
aboudh2 <46734372+aboudh2@users.noreply.github.com>
2019-02-27 03:14:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,18 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "-"
-
+  
 
 client.on('ready', () => {
-   console.log(----------------);
-      console.log(pixel Bot- Script By : Hamo-zine-lizer);
-        console.log(----------------);
-      console.log(ON ${client.guilds.size} Servers '     Script By : Zine-Hamo-lizer ');
-    console.log(----------------);
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(-help | ${client.guilds.size} Servers,"http://twitch.tv/Pixel Bot")
+   console.log(`----------------`);
+      console.log(`pixel Bot- Script By : Hamo-zine-lizer`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Zine-Hamo-lizer ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`-help | ${client.guilds.size} Servers `,"http://twitch.tv/Pixel Bot")
 client.user.setStatus("dnd")
 });
 
-}); client.on('message', msg => { if (msg.content === 'باك') { msg.reply('**ولكم منور يا عسل**'); } });
 
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN); 
