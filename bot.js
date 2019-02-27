ca463238837e3ea72ace57bad7cbc55876d12f66
aboudh2 <46734372+aboudh2@users.noreply.github.com>
2019-02-27 03:50:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,9 +16,64 @@ client.user.setStatus("dnd")
 
 client.on('message', message => {
     if (message.content === 'باك') {
-        message.reply('ولكم');
+        message.reply('ۏڷــګــمۘ ۛ ּمۘــڼۨــﯡڕ ּڀــ؏ــڛۜــڵ:heart:️
+');
       }
 });
 
+client.on('message', message => {
+    if (message.content === 'الرابط') {
+        message.reply('تفضل حب❤️ :-https://discord.gg/remJBW5');
+      }
+});
+      
+      
+
+client.on('message', message => {
+    if (message.content === 'الشعار') {
+        message.reply('!Discord | ! нσ‘,
+');
+      }
+});
+      
+      
+client.on('message', message => {
+    if (message.content === 'الشعار') {
+        message.reply('!Game | !♚`ӈơ | ');
+      }
+});
+      client.on('message', message => {
+    if (message.content === '! ') {
+        message.reply('wëlċömë tö ċlän thë höüsë öf hëll🎊🎉');
+      }
+});
+  
+     
+client.on('message', message => {
+    if (message.content === 'السلام عليكم') {
+        message.reply('عليكم السلام ورحمة الله وبركاته ❤️');
+      }
+});
+  
+     
+client.on('message', message => {
+    if (message.content === 'برب') {
+        message.reply(' ٺــۑْۧــټ ̨ﻻ̍ۙ ̨ٺــطــﯡڶ ؏ــڸــٻۧــڼۨــٰ̍ا̍😢
+[user] ');
+      }
+});
+  
+     
+client.on('message', message => {
+    if (message.content === '.') {
+        message.reply('آطلق إثبات وجودك💔😊
+[user] ');
+      }
+});
+  
+     
+    
+      
+      
 
 client.login(process.env.BOT_TOKEN); 
