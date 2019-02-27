c2fd057648384f9e6f8e354cf63b47a17a17cbc7
aboudh2 <46734372+aboudh2@users.noreply.github.com>
2019-02-27 03:29:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,5 +14,11 @@ client.user.setGame(`-help | ${client.guilds.size} Servers `,"http://twitch.tv/P
 client.user.setStatus("dnd")
 });
 
+client.on('message', message => {
+    if (message.content === 'باك') {
+        message.reply('ولكم');
+      }
+});
+
 
 client.login(process.env.BOT_TOKEN); 
